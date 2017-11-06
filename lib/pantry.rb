25f9fd9f0c119aeed67a423e1da2c09eac22dca5
TMad <madsen.tyler@gmail.com>
2017-11-06 13:15:43 -0700

    add stock check, restock, and part of shoping list

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,23 @@
 class Pantry
 
+  attr_reader :stock, :shopping_list
+
+  def initialize
+    @stock = Hash.new(0)
+    @shopping_list = Hash.new(0)
+  end
+
+  def stock_check(ingredient)
+    stock[ingredient]
+  end
+
+  def restock(ingredient, amount)
+    stock[ingredient] += amount
+  end
+
+  def add_to_shopping_list(recipe)
+    recipe.ingredients.reduce({}) do |result, (ingredient, amount)|
+      shopping_list += result[ingredient] =>
+
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,61 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/recipe'
 
 class PantryTest < Minitest::Test
+
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_pantry_has_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_pantry_can_check_stock_of_item_that_doesnt_exist
+    pantry = Pantry.new
+
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_pantry_can_check_stock_of_item_thats_in_stock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_pantry_stock_builds_on_itself
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+
+    pantry.restock("Cheese", 20)
+
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_add_ingredients_to_the_shopping_list
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+
+    assert_equal ({}), r.ingredients
+
+    r.add_ingredient("Cheese", 20)
+    r.add_ingredient("Flour", 20)
+
+    assert_equal ({"Cheese" => 20, "Flour" => 20}), r.ingredients
+
+    pantry.add_to_shopping_list(r)
+
+    assert_equal ({"Cheese" => 20, "Flour" => 20}), pantry.shopping_list
+  end
 
 end
